dba35586e4038e4711ff993c8c0fbcbbe32c4691
akafl916 <awesomekidaryan@gmail.com>
2022-02-04 13:31:04 -0500

    added stuff

--- Assets/Scripts/Main/Actors/Player.cs
+++ Assets/Scripts/Main/Actors/Player.cs
@@ -7,18 +7,35 @@ public class Player : Actor
     // Player Booleans
 
     private bool isJumping;
+    private bool hasLost;
+
+    // Player Number Vars
+    
+    private int health = 20;
+    private bool losthealth = false;
 
     // Player Input Vars
 
     private float horizontalMove;
 
+    // Player Inventory
+
+    private List<GameObject> inventory = new List<GameObject>(); // List of the player's auired GameObjects
+
     // Player Character Controller
 
-    public CharacterController2D controller;
+    public CharacterController2D controller; // Gets the movement controller
 
     // Player Static Variables
 
-    private static float playerRunSpeed = 50;
+    private static float playerRunSpeed = 50; // Sets the player's run speed
+
+    void addToInventory(GameObject item) { // Adds a given GameObject to a player
+        if(!(item.gameObject.tag == "collectable")) {
+            Debug.Log("wtf");
+        }
+        inventory.Add(item);
+    }
 
     void checkInput() // Checks for Input
     {
@@ -30,21 +47,9 @@ public class Player : Actor
         }
     }
 
-    void changeAnimation()
-    {
-        if(isJumping)
-        {
-            
-        }
-        else if(horizontalMove > 0)
-        {
-
-        } else if(horizontalMove < 0)
-        {
-
-        } else
-        {
-
+    void checkStatus() { // Checks the player's status
+        if(health <= 0) {
+            Lose();
         }
     }
 
@@ -54,14 +59,21 @@ public class Player : Actor
         {
             isOnGround = true;
         }
+        if (col.gameObject.tag == "Orphan") {
+            health--;
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D col) // Runs when this GameObject exits collision
+    void OnCollisionExit2D(Collision2D col) // Runs when this GameObject exits collision
     {
         if (col.gameObject.tag == "Ground")
         {
             isOnGround = false;
         }
+    }
+
+    void Lose() { // Lose is called when the player has 0 or less health.
+        Debug.Log("lmao you're trash");
     }
 
     // Start is called before the first frame update
@@ -75,6 +87,11 @@ public class Player : Actor
     {
         // Check for input
         checkInput();
+
+        // Check for status
+        checkStatus();
+
+        Debug.Log(losthealth);
 
     }
 
